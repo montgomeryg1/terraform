c6b148f6b08045180844bb051f673f5a2e49640a
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-03 16:30:47 +0000

    Update resources.tf

--- k8s/resources.tf
+++ k8s/resources.tf
@@ -11,12 +11,11 @@ resource "azurerm_resource_group" "example" {
   location = local.location
 }
 
-
 resource "azurerm_virtual_network" "example" {
   name                = "${local.prefix}-network"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.1.0.0/16"]
+  address_space       = module.variables.vnet_address_space
 }
 
 resource "azurerm_subnet" "example" {
