9bc65e88cf55ade05d3fc352b5554882a4eb3951
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-03 16:11:41 +0000

    Update variables.tf

--- k8s/variables.tf
+++ k8s/variables.tf
@@ -39,6 +39,3 @@ variable "public_ssh_key_path" {
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA1JyyXQle9w4lUoDI6N7HhS0/cs6RyIBH6kFX54YeHJkpzRHBdCmy3PAVNtfX+j49WtuNMKuByNQV9iEpShLA8idkqrVmkw/UXJ8DJQr9lGiu3bwRcg3/6gC6/WB5rLaExwlri4KndPgMUSJimdDk7jJWCTPQ3PMiHfWBr0ZLcgPvZT4ZLwzhieuKaAxxoqu39ZXy2OL/XuQVpPePglucAItRwZl0YvSp4rwJsV3wQAl6Skybj+w5DEK4ttdIgLaZw60KAfrki5Z2gbMznvKWOqLGXEtw1ter4/+IEEDTNZAw+a7l+H4886zQC/2aOXNsqKBrJrotcMRKPJQxe2C8Nw== rsa-key-20200129"
 }
 
-variable "prefix" {
-  default = "wgm"
-}
