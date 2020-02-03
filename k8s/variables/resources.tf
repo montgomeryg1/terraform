bf342dfba5cd4aa4080f7877814b7aa99c43d909
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-03 16:41:29 +0000

    Update resources.tf

--- k8s/variables/resources.tf
+++ k8s/variables/resources.tf
@@ -10,7 +10,7 @@ variable "vnet_address_space_map" {
 }
 
 output "vnet_address_space" {
-  value = ["${split(",", var.vnet_address_space_map[var.environment])}”]
+  value = ["${split(",", var.vnet_address_space_map[var.environment])}"]
 }
 
 variable "subnet_map" {
