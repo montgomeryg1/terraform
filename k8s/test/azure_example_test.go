260a6066ec0689f383d9de9d3acc6560d7601c7f
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-04 14:26:47 +0000

    Update azure_example_test.go

--- k8s/test/azure_example_test.go
+++ k8s/test/azure_example_test.go
@@ -31,7 +31,7 @@ func TestTerraformAzure(t *testing.T) {
 	terraform.Init(t, tfOptions)
 	terraform.RunTerraformCommand(t, tfOptions, terraform.FormatArgs(tfOptions, "plan", "-out="+tfPlanOutput)...)
 	
-	clusterUsername := terraform.Output(t, terraformOptions, "cluster_username")
+	clusterUsername := terraform.Output(t, tfOptions, "cluster_username")
 	// Validate the test result
 	// for _, mod := range plan.Diff.Modules {
 		// fmt.Println(mod.Path)
