2be39cfa5ee462c28579091e679e7f18ecb37c65
George Montgomery <montgomery.wg@gmail.com>
2020-02-04 22:53:45 +0000

    Updated test.go

--- test/azure_example_test.go
+++ test/azure_example_test.go
@@ -10,8 +10,8 @@ import (
 	// "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-06-01/compute"
 	// "github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	//terraformCore "github.com/hashicorp/terraform/terraform"
-	terraformCore "github.com/hashicorp/terraform/plans"
+	terraformCore "github.com/hashicorp/terraform/terraform"
+	// terraformCore "github.com/hashicorp/terraform/plans"
 )
 
 func TestTerraformAzure(t *testing.T) {
@@ -45,7 +45,7 @@ func TestTerraformAzure(t *testing.T) {
 				t.Fatal(err)
 			}
 			defer f.Close()
-			plan, err := ReadPlan(f)
+			plan, err := terraformCore.ReadPlan(f)
 			if err != nil {
 				t.Fatal(err)
 			}
