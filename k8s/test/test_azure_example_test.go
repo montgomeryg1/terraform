1d785a4dbcc65c8ee59e2f96845c382ec828eaa4
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-04 11:43:06 +0000

    Update test_azure_example_test.go

--- k8s/test/test_azure_example_test.go
+++ k8s/test/test_azure_example_test.go
@@ -3,13 +3,13 @@ package test
 import (
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-06-01/compute"
-	"github.com/gruntwork-io/terratest/modules/azure"
+	// "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-06-01/compute"
+	// "github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/assert"
 )
 
-func TestTerraformAzureExample(t *testing.T) {
+func TestTerraformAzure(t *testing.T) {
 	t.Parallel()
 
 	// website::tag::1:: Configure Terraform setting up a path to Terraform code.
@@ -22,14 +22,64 @@ func TestTerraformAzureExample(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 
 	// website::tag::2:: Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
-	terraform.InitAndApply(t, terraformOptions)
+	// terraform.InitAndApply(t, terraformOptions)
+	terraform.InitAndPlan(t, terraformOptions)
 
 	// website::tag::3:: Run `terraform output` to get the values of output variables
-	vmName := terraform.Output(t, terraformOptions, "vm_name")
-	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	// vmName := terraform.Output(t, terraformOptions, "vm_name")
+	// resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
 	// website::tag::4:: Look up the size of the given Virtual Machine and ensure it matches the output.
-	actualVMSize := azure.GetSizeOfVirtualMachine(t, vmName, resourceGroupName, "")
-	expectedVMSize := compute.VirtualMachineSizeTypes("Standard_B1s")
-	assert.Equal(t, expectedVMSize, actualVMSize)
+	// actualVMSize := azure.GetSizeOfVirtualMachine(t, vmName, resourceGroupName, "")
+	// expectedVMSize := compute.VirtualMachineSizeTypes("Standard_B1s")
+	// assert.Equal(t, expectedVMSize, actualVMSize)
+}
+
+func TestStorageAccountName(t *testing.T) {
+	t.Parallel()
+
+	// Test cases for storage account name conversion logic
+	testCases := map[string]string{
+		"TestWebsiteName": "testwebsitenamedata001",
+		"ALLCAPS":         "allcapsdata001",
+		"S_p-e(c)i.a_l":   "specialdata001",
+		"A1phaNum321":     "a1phanum321data001",
+		"E5e-y7h_ng":      "e5ey7hngdata001",
+	}
+
+	for input, expected := range testCases {
+		// Specify the test case folder and "-var" options
+		tfOptions := &terraform.Options{
+			TerraformDir: "../",
+			Vars: map[string]interface{}{
+				"website_name": input,
+			},
+		}
+
+		// Terraform init and plan only
+		tfPlanOutput := "terraform.tfplan"
+		terraform.Init(t, tfOptions)
+		terraform.RunTerraformCommand(t, tfOptions, terraform.FormatArgs(tfOptions, "plan", "-out="+tfPlanOutput)...)
+
+		// Read and parse the plan output
+		f, err := os.Open(path.Join(tfOptions.TerraformDir, tfPlanOutput))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		plan, err := terraformCore.ReadPlan(f)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Validate the test result
+		for _, mod := range plan.Diff.Modules {
+			if len(mod.Path) == 2 && mod.Path[0] == "root" && mod.Path[1] == "staticwebpage" {
+				actual := mod.Resources["azurerm_storage_account.main"].Attributes["name"].New
+				if actual != expected {
+					t.Fatalf("Expect %v, but found %v", expected, actual)
+				}
+			}
+		}
+	}
 }
