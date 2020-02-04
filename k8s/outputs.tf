f44064ec62c5857b6291a30401e03bf0bb213d18
George Montgomery <George.Montgomery@nttdata.com>
2020-02-04 14:42:32 +0000

    Updated outputs.tf

--- k8s/outputs.tf
+++ k8s/outputs.tf
@@ -25,3 +25,11 @@ output "cluster_password" {
 output "host" {
   value = azurerm_kubernetes_cluster.example.kube_config.0.host
 }
+
+output "fqdn" {
+  value = azure_kubernetes_cluster.example.kube_config.0.fqdn
+}
+
+output "node_resource_group" {
+  value = azure_kubernetes_cluster.example.kube_config.0.node_resource_group
+}
