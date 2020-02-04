8b2c604de026ec446a3d92c3ca9b8568669c2eac
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-04 14:25:29 +0000

    Update outputs.tf

--- k8s/outputs.tf
+++ k8s/outputs.tf
@@ -1,14 +1,14 @@
-output "client_key" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_key
-}
+# output "client_key" {
+  # value = azurerm_kubernetes_cluster.example.kube_config.0.client_key
+# }
 
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
-}
+# output "client_certificate" {
+  # value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+# }
 
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
-}
+# output "cluster_ca_certificate" {
+  # value = azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
+# }
 
 output "cluster_username" {
   value = azurerm_kubernetes_cluster.example.kube_config.0.username
@@ -18,10 +18,18 @@ output "cluster_password" {
   value = azurerm_kubernetes_cluster.example.kube_config.0.password
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
-}
+# output "kube_config" {
+  # value = azurerm_kubernetes_cluster.example.kube_config_raw
+# }
 
 output "host" {
   value = azurerm_kubernetes_cluster.example.kube_config.0.host
+}
+
+output "fqdn" {
+	value = azure_kubernetes_cluster.example.fqdn
+}
+
+output "node_resource_group" {
+	value = azure_kubernetes_cluster.example.node_resource_group
 }
