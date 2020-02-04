d1b7082029a69bcd6714f3294066350ce5281148
George Montgomery <montgomery.wg@gmail.com>
2020-02-04 22:46:21 +0000

    Updated test.go

--- test/azure_example_test.go
+++ test/azure_example_test.go
@@ -45,7 +45,7 @@ func TestTerraformAzure(t *testing.T) {
 				t.Fatal(err)
 			}
 			defer f.Close()
-			plan, err := terraformCore.ReadPlan(f)
+			plan, err := ReadPlan(f)
 			if err != nil {
 				t.Fatal(err)
 			}
