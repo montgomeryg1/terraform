3b1773076b1224b2e42f821cea7550511608a76b
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-06 13:43:03 +0000

    Update locals.tf

--- k8s/locals.tf
+++ k8s/locals.tf
@@ -2,5 +2,5 @@ locals {
   environment = "${lookup(var.workspace_to_environment_map, terraform.workspace, "dev")}"
   size        = "${local.environment == "dev" ? lookup(var.workspace_to_size_map, terraform.workspace, "small") : var.environment_to_size_map[local.environment]}"
   prefix      = "k8sqt"
-  location    = "northeurope"
+  region    = "northeurope"
 }
