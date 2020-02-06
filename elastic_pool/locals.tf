7ee713c3812f99af2332df55b03b546309278a84
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-06 10:30:37 +0000

    Create locals.tf

---
+++ elastic_pool/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  environment = "${lookup(var.workspace_to_environment_map, terraform.workspace, "dev")}"
+  size        = "${local.environment == "dev" ? lookup(var.workspace_to_size_map, terraform.workspace, "small") : var.environment_to_size_map[local.environment]}"
+  region      = var.region
+}
