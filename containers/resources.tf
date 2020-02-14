663cf640b3196a771f60fee1dc4063077f16c199
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-14 14:47:53 +0000

    Update resources.tf

--- containers/resources.tf
+++ containers/resources.tf
@@ -37,8 +37,8 @@ resource "azurerm_container_registry" "sandbox" {
 
 resource "azurerm_container_group" "sandbox" {
   name                = "${random_string.sandbox.result}-continst"
-  location            = "${azurerm_resource_group.sandbox.location}"
-  resource_group_name = "${azurerm_resource_group.sandbox.name}"
+  location            = azurerm_resource_group.sandbox.location
+  resource_group_name = azurerm_resource_group.sandbox.name
   ip_address_type     = "public"
   os_type             = "linux"
 
