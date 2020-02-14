789152a118b7ec9bb46c8e8b20f7395bd1f1a012
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-14 14:59:51 +0000

    Update azure_test.go

--- test/azure_test.go
+++ test/azure_test.go
@@ -11,24 +11,14 @@ import (
 	// "github.com/stretchr/testify/require"
 )
 
-
-func TestFormat(t *testing.T) {
-	dir := "../."
-	tfOptions := &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: dir,
-	}
-	defer terraform.Destroy(t, tfOptions)
-
-	// Terraform format
-	terraform.RunTerraformCommand(t, tfOptions, terraform.FormatArgs(tfOptions, "fmt")...)
-}
-
 func TestVnet(t *testing.T) {
 	dir := "../vnet"
 	tfOption := &terraform.Options{
 		// The path to where our Terraform code is located
 		TerraformDir: dir,
+		Vars: map[string]interface{}{
+			"region": "westeurope",
+		},
 	}
 	defer terraform.Destroy(t, tfOption)
 
@@ -88,52 +78,4 @@ func TestK8s(t *testing.T) {
 	// Test that the Node count matches the Terraform specification
 	// assert.Equal(t, int32(expectedAgentCount), actualCount)
 }
-
-// func TestStorageAccountName(t *testing.T) {
-// 	t.Parallel()
-
-// 	// Test cases for storage account name conversion logic
-// 	testCases := map[string]string{
-// 		"TestWebsiteName": "testwebsitenamedata001",
-// 		"ALLCAPS":         "allcapsdata001",
-// 		"S_p-e(c)i.a_l":   "specialdata001",
-// 		"A1phaNum321":     "a1phanum321data001",
-// 		"E5e-y7h_ng":      "e5ey7hngdata001",
-// 	}
-
-// 	for input, expected := range testCases {
-// 		// Specify the test case folder and "-var" options
-// 		tfOptions := &terraform.Options{
-// 			TerraformDir: "../",
-// 			Vars: map[string]interface{}{
-// 				"website_name": input,
-// 			},
-// 		}
-
-// 		// Terraform init and plan only
-// 		tfPlanOutput := "terraform.tfplan"
-// 		terraform.Init(t, tfOptions)
-// 		terraform.RunTerraformCommand(t, tfOptions, terraform.FormatArgs(tfOptions, "plan", "-out="+tfPlanOutput)...)
-
-// 		// Read and parse the plan output
-// 		f, err := os.Open(path.Join(tfOptions.TerraformDir, tfPlanOutput))
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-// 		defer f.Close()
-// 		plan, err := terraformCore.ReadPlan(f)
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-
-// 		// Validate the test result
-// 		for _, mod := range plan.Diff.Modules {
-// 			if len(mod.Path) == 2 && mod.Path[0] == "root" && mod.Path[1] == "staticwebpage" {
-// 				actual := mod.Resources["azurerm_storage_account.main"].Attributes["name"].New
-// 				if actual != expected {
-// 					t.Fatalf("Expect %v, but found %v", expected, actual)
-// 				}
-// 			}
-// 		}
-// 	}
 // }
