386aba556decdb5fa0b0bd592e4cfc7b7cfe2c35
montgomeryg1 <montgomeryg1@users.noreply.github.com>
2020-02-14 14:09:55 +0000

    Apply formatting changes

--- vnet/resources.tf
+++ vnet/resources.tf
@@ -17,7 +17,7 @@ resource "azurerm_network_security_group" "nsg1" {
   location            = azurerm_resource_group.sandbox.location
   resource_group_name = azurerm_resource_group.sandbox.name
 }
-  
+
 resource "azurerm_network_security_rule" "nsr1" {
   name                        = "allow-ssh"
   priority                    = 100
@@ -30,8 +30,8 @@ resource "azurerm_network_security_rule" "nsr1" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.sandbox.name
   network_security_group_name = azurerm_network_security_group.nsg1.name
-}  
-  
+}
+
 resource "azurerm_network_security_rule" "nsr2" {
   name                        = "allow-http/s"
   priority                    = 110
@@ -44,7 +44,7 @@ resource "azurerm_network_security_rule" "nsr2" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.sandbox.name
   network_security_group_name = azurerm_network_security_group.nsg1.name
-}    
+}
 
 resource "azurerm_virtual_network" "sandbox" {
   name                = "virtualNetwork1"
@@ -74,4 +74,4 @@ resource "azurerm_subnet_network_security_group_association" "sandbox" {
   network_security_group_id = azurerm_network_security_group.sandbox.id
 }
 
-  
+
