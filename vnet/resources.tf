124e42f698fb66efc900b842ceb972f165cd75b7
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-14 12:21:08 +0000

    Update resources.tf

--- vnet/resources.tf
+++ vnet/resources.tf
@@ -38,3 +38,10 @@ resource "azurerm_subnet" "sandbox" {
   resource_group_name  = azurerm_resource_group.sandbox.name
   virtual_network_name = azurerm_virtual_network.sandbox.name
 }
+
+  
+resource "azurerm_subnet_network_security_group_association" "sandbox" {
+  for_each                  = module.variable.subnets
+  subnet_id                 = azurerm_subnet.sandbox[each.key].id
+  network_security_group_id = azurerm_network_security_group.sandbox.id
+}
