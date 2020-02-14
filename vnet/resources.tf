ab7185d7c9dff364cdfeb6215b153e3ad2134277
montgomeryg1 <montgomery.wg@gmail.com>
2020-02-14 14:09:02 +0000

    Update resources.tf

--- vnet/resources.tf
+++ vnet/resources.tf
@@ -12,11 +12,39 @@ resource "azurerm_resource_group" "sandbox" {
   location = var.region
 }
 
-resource "azurerm_network_security_group" "sandbox" {
-  name                = "${local.environment}-networkSecurityGroup1"
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "networkSecurityGroup1"
   location            = azurerm_resource_group.sandbox.location
   resource_group_name = azurerm_resource_group.sandbox.name
 }
+  
+resource "azurerm_network_security_rule" "nsr1" {
+  name                        = "allow-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.sandbox.name
+  network_security_group_name = azurerm_network_security_group.nsg1.name
+}  
+  
+resource "azurerm_network_security_rule" "nsr2" {
+  name                        = "allow-http/s"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80,443"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.sandbox.name
+  network_security_group_name = azurerm_network_security_group.nsg1.name
+}    
 
 resource "azurerm_virtual_network" "sandbox" {
   name                = "virtualNetwork1"
@@ -45,3 +73,5 @@ resource "azurerm_subnet_network_security_group_association" "sandbox" {
   subnet_id                 = azurerm_subnet.sandbox[each.key].id
   network_security_group_id = azurerm_network_security_group.sandbox.id
 }
+
+  
