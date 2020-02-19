7b64971119f013432f52236862256204bff2d553
George Montgomery <George.Montgomery@nttdata.com>
2020-02-19 16:33:59 +0000

    Updated outputs.tf

--- ubuntu_vm/outputs.tf
+++ ubuntu_vm/outputs.tf
@@ -6,14 +6,14 @@ output "resource_group_name" {
   value = azurerm_resource_group.testing.name
 }
 
-data "azurerm_public_ip" "ubuntuvm" {
-  name                = "${var.prefix}-pip"
-  resource_group_name = azurerm_resource_group.testing.name
-}
+# data "azurerm_public_ip" "ubuntuvm" {
+#   name                = azurerm_public_ip.ubuntuvm.name
+#   resource_group_name = azurerm_resource_group.testing.name
+# }
 
-output "public_ip_address" {
-  value = data.azurerm_public_ip.ubuntuvm.ip_address
-}
+# output "public_ip_address" {
+#   value = data.azurerm_public_ip.ubuntuvm.ip_address
+# }
 
 output "public_ip_name" {
   value = azurerm_public_ip.ubuntuvm.name
