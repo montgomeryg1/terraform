3c813d135901a3d739866f0e36488deb61da79d2
George Montgomery <George.Montgomery@nttdata.com>
2020-02-21 10:01:18 +0000

    Updated variables module

--- ubuntu_vm/main.tf
+++ ubuntu_vm/main.tf
@@ -13,8 +13,8 @@ terraform {
 
 
 module "variables" {
-  source = "github.com/montgomeryg1/terraform//variables"
-  # source      = "./variables"
+  # source = "github.com/montgomeryg1/terraform//variables"
+  source      = "./variables"
   environment = local.environment
   size        = local.size
   region      = var.region
