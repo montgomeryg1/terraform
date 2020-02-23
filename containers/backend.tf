f9aab20a890c36385e4b0ba9b3fd33558f62db12
George Montgomery <montgomery.wg@gmail.com>
2020-02-23 15:31:41 +0000

    Updated backend

--- containers/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name = artifacts
-        storage_account_name = artifactsdevelopment
-        container_name = tf-statefiles
-        key = tfstate 
-        access_key = var.access_key
-    }
-}
