fb05074e5eadf984fe8b9b0f0da350decc166a84
George Montgomery <montgomery.wg@gmail.com>
2020-02-23 06:30:43 +0000

    Added variables

--- containers/variables.tf
+++ containers/variables.tf
@@ -1,3 +1,12 @@
+variable "subscription_id" {}
+
+variable "client_id" {}
+
+variable "client_secret" {}
+
+variable "tenant_id" {}
+
+
 variable "workspace_to_environment_map" {
   type = map
   default = {
