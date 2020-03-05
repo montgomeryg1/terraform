c0dacc9b17ac0fcb38081b63fa2a51a243f0ff68
George <montgomery.wg@gmail.com>
2020-03-05 09:33:11 +0000

    Updated app_service terraform

--- app_service/resources.tf
+++ app_service/resources.tf
@@ -1,6 +1,14 @@
+module "variables" {
+  # source = "github.com/montgomeryg1/terraform//variables"
+  source      = "../variables"
+  environment = local.environment
+  size        = local.size
+  region      = var.region
+}
+
 resource "azurerm_resource_group" "testing" {
-  name     = "api-rg-pro"
-  location = "West Europe"
+  name     = "testing-resources"
+  location = var.region
 }
 
 resource "azurerm_app_service_plan" "testing" {
@@ -9,7 +17,29 @@ resource "azurerm_app_service_plan" "testing" {
   resource_group_name = azurerm_resource_group.testing.name
 
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = module.variables.app_service_plan_tier
+    size = module.variables.app_service_plan_size
+  }
+}
+
+resource "azurerm_app_service" "testing" {
+  name                = "testing-app-service"
+  location            = azurerm_resource_group.testing.location
+  resource_group_name = azurerm_resource_group.testing.name
+  app_service_plan_id = azurerm_app_service_plan.testing.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
 }
--- app_service/variables.tf
+++ app_service/variables.tf
@@ -1,3 +1,45 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# WORKSPACE VARIABLES
+# Define workspace variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "workspace_to_environment_map" {
+  type = map
+  default = {
+    dev     = "dev"
+    qa      = "qa"
+    staging = "staging"
+    prod    = "prod"
+  }
+}
+
+variable "environment_to_size_map" {
+  type = map
+  default = {
+    dev     = "small"
+    qa      = "medium"
+    staging = "large"
+    prod    = "xlarge"
+  }
+}
+
+variable "workspace_to_size_map" {
+  type = map
+  default = {
+    dev = "small"
+  }
+}
+
+variable "region" {
+  default = "northeurope"
+}
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
 variable "subscription_id" {}
 
 variable "client_id" {}
