4d827378c2f7bd96530c5cdf303ab048b3ecda2d
George <montgomery.wg@gmail.com>
2020-03-06 16:13:53 +0000

    Updated backend.tf

--- ubuntuvm/backend.tf
+++ ubuntuvm/backend.tf
@@ -4,6 +4,6 @@ terraform {
         storage_account_name = "__storageacct__"
         container_name = "tf-statefiles"
         key = "__terraform.name__"
-        access_key = "__access_key__"
+        access_key = "__accesskey__"
     }
 }
---
+++ ubuntuvm/terraform.tfvars
@@ -0,0 +1,4 @@
+tenant_id = "__tenantID__"
+subscription_id = "__subscriptionID__"
+client_id = "__applicationID__"
+client_secret = "__applicationSecret__"
